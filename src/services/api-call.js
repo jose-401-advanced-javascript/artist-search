e84eeca81053d6e4fd43d8f4b5d9210556ca65e2
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-05 14:06:25 -0800

    kam - api call and search form

--- src/services/api-call.js
+++ src/services/api-call.js
@@ -2,7 +2,10 @@ export const getArtists = (search) => {
   return fetch(`http://musicbrainz.org/ws/2/artst?query=${search}&fmt=json&limit=25`)
     .then(res => res.json())
     .then(({ results: artists }) => artists.map(artist => ({
-      
+      id: artist.id,
+      name: artist.name,
+      country: artist.area.country,
+      tags: [artist.tags[0].name, artist.tags[1].name, artist.tags[2].name]
     })
-    }))
+    ));
 };
