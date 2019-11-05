4c0cdc283f38724a2ff4975f6b6d53c4c871729a
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-05 14:30:38 -0800

    Jose - kept working on Main.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Main from './containers/Main ';
+import SearchForm from './artists/SearchForm';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <SearchForm />
+    </>
+  )
 }
--- src/components/containers/Main .js
+++ src/components/containers/Main .js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Artists from '../../components/artists/Artists';
+import Search
 import { getArtists } from '../../services/api-call';
 
 export default class Main extends Component {
@@ -15,14 +17,22 @@ export default class Main extends Component {
 
   handleOnClick() {
     getArtists(this.state.query)
-    .then()
+      .then(artists => this.setState({ artists }));
+    console.log(this.state.artists);
+
+  }
+
+  handleSubmit() {
+    event.preventDefault();
   }
 
   render() {
-
+    const { artists } = this.state;
     return (
-
-    )
+      <>
+        <Artists artists={artists} />
+      </>
+    );
   }
 
 }
