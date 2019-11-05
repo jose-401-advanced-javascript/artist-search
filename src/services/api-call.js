f877c9c39286e96c96024e809820875fea9033e4
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-05 15:33:53 -0800

    Jose - got getArtists to work

--- src/services/api-call.js
+++ src/services/api-call.js
@@ -2,14 +2,12 @@ export const getArtists = (search) => {
   return fetch(`http://musicbrainz.org/ws/2/artist?query=${search}&fmt=json&limit=25`)
     .then(res => res.json())
     .then(results => {
-      console.log(results);
       return results.artists.map(artist => {
-        console.log(artist);
         return {
           id: artist.id,
           name: artist.name || '',
           country: artist.area ? artist.area.name : '',
-          tags: artist.tags ? [artist.tags[0].name || '', artist.tags[1].name || '', artist.tags[2].name || ''] : ['']
+          tags: (artist.tags || []).map(tag => tag.name).slice(0, 3)
         };
       });
     });
