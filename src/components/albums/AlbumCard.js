872d64d0a7b125deb6d59aa9b6ee6170816c4941
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-05 16:25:05 -0800

    kam - album card

---
+++ src/components/albums/AlbumCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ArtistCard from '../artists/ArtistCard';
+
+const AlbumCard = ({ title, id, }) => {
+
+return(
+
+)
+
+};
+
+AlbumCard.propTypes = {
+
+};
+
+export default ArtistCard;
