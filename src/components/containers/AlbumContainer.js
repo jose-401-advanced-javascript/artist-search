7c160a2d43d5b2a54f702994d9433561339905a3
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-06 14:39:47 -0800

    kam - api call work

--- src/components/containers/AlbumContainer.js
+++ src/components/containers/AlbumContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Albums from '../albums/Albums';
 import { getAlbums, getAlbumArt } from '../../services/api-call';
-import ArtistCard from '../artists/ArtistCard';
 
 export default class AlbumContainer extends Component {
 
@@ -18,24 +17,27 @@ export default class AlbumContainer extends Component {
     albums: []
   }
 
-  addAlbumArt = (arr) => {
-    return arr.map(album => {
-      const coverArt = getAlbumArt(album.id);
-      return {
-        id: album.id,
-        title: album.title,
-        cover: coverArt
-      };
-    });
-  }
+  // addAlbumArt = (arr) => {
+  //   return Promise.all(arr.map(album => {
+  //     getAlbumArt(album.id)
+  //       .then(cover => {
+  //         return {
+  //           id: album.id,
+  //           title: album.title,
+  //           cover: cover
+  //         };
+  //       }));
+  //   }));
+  // }
 
   componentDidMount() {
     getAlbums(this.props.match.params.id)
       .then(albums => {
-        this.setState({ albums: this.addAlbumArt(albums) });
+        this.setState({ albums });
         console.log(this.state.albums);
       });
   }
+
 
   render() {
 
