60a7ed5b1b7713279bb29c98b316929eb4f9b64c
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-06 09:26:56 -0800

    trying to pass travis

--- src/components/containers/AlbumContainer.js
+++ src/components/containers/AlbumContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Albums from '../albums/Albums';
 import { getAlbums } from '../../services/api-call';
 
